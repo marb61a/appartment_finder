7573a0dbd7f653224f421927e37bb869ce7a4811
marb <marb61a@gmail.com>
2015-11-19 09:01:32 +0000

    added routes to rental.js

--- app/rentals/rentals.js
+++ app/rentals/rentals.js
@@ -6,9 +6,31 @@ angular.module('rentfinds.rentals', ['ngRoute'])
   $routeProvider.when('/rentals', {
     templateUrl: 'rentals/rentals.html',
     controller: 'RentalsCtrl'
-  });
+  }).
+      when('/details/:id', {
+      templateUrl: 'rentals/details.html',
+      controller: 'DetailsCtrl'
+  }).
+      when('/add', {
+      templateUrl: 'rentals/add.html',
+      controller: 'RentalsCtrl'
+  }).
+    	when('/edit/:id', {
+      templateUrl: 'rentals/edit.html',
+      controller: 'EditCtrl'
+  })
+  
+  
 }])
 
 .controller('RentalsCtrl', [function() {
 	console.log('RentalsCtrl Working..');
-}]);
+}])
+
+.controller('DetailsCtrl', [function() {
+	console.log('DetailsCtrl Working..');
+}])
+
+.controller('EditCtrl', [function() {
+	console.log('EditCtrl Working..');
+}])
